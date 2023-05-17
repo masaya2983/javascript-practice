2288922c441d369646e36d3a41c23eb231df282e
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-17 11:40:04 +0000

    コメント

---
+++ app.js
@@ -0,0 +1,16 @@
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num<max){
+  num = num * 2;
+  count = count + 1;
+}
+
+let i;
+let num = 0;
+
+for(i= 1; i< 11; i++){
+  num = num + i;
+}
+alter('1から10まで足し算した結果は' + num + 'です');
